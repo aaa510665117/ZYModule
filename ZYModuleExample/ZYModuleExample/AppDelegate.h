6513dd1f64be461b292ffe97eb9d272cb4a04876
ZY <510665117@qq.com>
2018-02-28 14:57:43 +0800

    增加ZYModule示例

---
+++ ZYModuleExample/ZYModuleExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYModuleExample
+//
+//  Created by ZY on 2018/2/28.
+//  Copyright © 2018年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYModuleExample/ZYModuleExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZYModuleExample
+//
+//  Created by ZY on 2018/2/28.
+//  Copyright © 2018年 ZY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
